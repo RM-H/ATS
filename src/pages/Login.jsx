87ab8786f58738976590eee1eb709a10279ff7d3
rm-h <raminhp1374@gmail.com>
2024-07-07 17:00:04 +0330

    dev :

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,16 +1,15 @@
 import {ErrorMessage, Field, Form, Formik} from "formik";
 import * as Yup from "yup";
-import  {useState} from 'react'
+import {useEffect, useState} from 'react'
 import {Spinner} from '../components/index.js'
 
 import CryptoJS from 'crypto-js'
 
 import {useNavigate} from "react-router-dom";
-import {getSms,verifySmsCode,getUserinfo} from '../services/service.js'
+import {getSms, verifySmsCode, getUserinfo} from '../services/service.js'
 import {toast, ToastContainer} from "react-toastify";
 import {useDispatch} from "react-redux";
-import {setuser,setstep} from '../slices/userSlice.js'
-
+import {setuser, setstep, setloading} from '../slices/userSlice.js'
 
 
 const Login = () => {
@@ -24,15 +23,15 @@ const Login = () => {
     const nav = useNavigate()
 
 
-    const HandleLogin =async (v) => {
+    const HandleLogin = async (v) => {
 
 
-        if (phoneentered===false) {
+        if (phoneentered === false) {
             const form = new FormData
-            form.append('phone',v.phone)
+            form.append('phone', v.phone)
             setLoading(true)
             const res = await getSms(form)
-            if (res.data.code==1){
+            if (res.data.code == 1) {
                 setphoneentered(true)
                 setLoading(false)
                 toast.success('کد ارسال شد')
@@ -41,62 +40,92 @@ const Login = () => {
             }
 
 
-
         } else {
             const form = new FormData
-            form.append('phone',v.phone)
-            form.append('code',v.code);
+            form.append('phone', v.phone)
+            form.append('code', v.code);
             setLoading(true)
             let resp = await verifySmsCode(form)
 
 
-          if (resp.data.code==1){
-              const hashed =CryptoJS.AES.encrypt(JSON.stringify(resp.data.user.token),'ats' , toString())
-              localStorage.setItem('user', hashed)
+            if (resp.data.code == 1) {
+                const hashed = CryptoJS.AES.encrypt(JSON.stringify(resp.data.user.token), 'ats', toString())
+                localStorage.setItem('user', hashed)
 
 
-              // user continues his steps
-              if (resp.data.user.departemant) {
-                  dispatch(setstep(1))
-              }
-              if (resp.data.user.name) {
-                  dispatch(setstep(2))
-              }
-
-              localStorage.setItem('user' ,crypto.AES.encrypt(resp.data.user.token,'ats'))
-
-              dispatch(setuser(resp.data))
-              setLoading(false)
+                dispatch(setuser(resp.data))
+                setLoading(false)
 
 
-              nav('/ats')
-              toast.success('با موفقیت وارد شدید')
-          } else {
-              setLoading(false)
-              setphoneentered(false)
-             toast.warning(resp.data.error)
-          }
+                nav('/ats')
+                toast.success('با موفقیت وارد شدید')
+            } else {
+                setLoading(false)
+                setphoneentered(false)
+                toast.warning(resp.data.error)
+            }
 
         }
 
     }
 
 
+    // checking to see if user had already logged in before
+    useEffect(() => {
+
+      
+        let user = localStorage.getItem('user')
+
+        if (user) {
+            let decrypt = JSON.parse(CryptoJS.AES.decrypt(localStorage.getItem('user'), 'ats').toString(CryptoJS.enc.Utf8));
 
 
-    // const developementgetdata = async () => {
-    //     const formdata = new FormData()
-    //     formdata.append('token' , '667cf1f5242cd')
-    //   const response = await getUserinfo(formdata)
-    //     if (response.data.code===1) {
-    //         const hashed =CryptoJS.AES.encrypt(JSON.stringify('667cf1f5242cd'),'ats' , toString())
-    //         localStorage.setItem('user', hashed)
-    //         dispatch(setuser(response.data))
-    //         nav('/ats')
-    //     }else {
-    //         console.log(response.data.error)
-    //     }
-    // }
+            const getuser = async () => {
+                dispatch(setloading(true))
+
+                const formdata = new FormData()
+                formdata.append('token', decrypt)
+                const response = await getUserinfo(formdata)
+                if (response.data.code == 1) {
+
+                    if (response.data.questions !== false) {
+                        dispatch(setstep(5))
+                    } else if (response.data.works !== false) {
+                        dispatch(setstep(4))
+
+                    } else if (response.data.education1 !== false || response.data.education2 !== false || response.data.education3 !== false || response.data.education4 !== false) {
+                        dispatch(setstep(3))
+
+                    } else if (response.data.user !== false) {
+                        dispatch(setstep(2))
+                    } else if (response.data.departemant) {
+                        dispatch(setstep(1))
+                    }
+
+
+                    console.log(response.data)
+                    dispatch(setuser(response.data))
+                    dispatch(setloading(false))
+
+                    nav('/ats')
+
+                } else {
+                    dispatch(setloading(false))
+
+                    toast.warning(response.data.error)
+                    nav('/')
+                }
+            }
+            getuser().then()
+
+
+        } else {
+            toast.warning('لطفا ابتدا وارد سیستم شوید.')
+            nav('/')
+        }
+
+
+    }, []);
 
 
     return (
@@ -104,20 +133,16 @@ const Login = () => {
 
             <div
 
-                 style={{width: "100wv", height: '100vh' , display:'flex', justifyContent:'center' , alignItems:'center' , backgroundImage:'url(/assets/images/wallpaper.jpg)' , backgroundSize:'cover' , backgroundRepeat:'no-repeat'}}>
-
-                {/*<button onClick={developementgetdata}>*/}
-                {/*    dev*/}
-                {/*</button>*/}
-
-
-
-
-
-
-
-
-
+                style={{
+                    width: "100wv",
+                    height: '100vh',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    backgroundImage: 'url(/assets/images/wallpaper.jpg)',
+                    backgroundSize: 'cover',
+                    backgroundRepeat: 'no-repeat'
+                }}>
 
 
                 <div className="main-login " style={{
@@ -128,7 +153,7 @@ const Login = () => {
 
                     <div className="signup is-size-5 has-text-centered yekan-regular">
                         <h2>
-                           به سامانه رهگیری استخدام (ATS) افق ایرانیان خوش آمدید.
+                            به سامانه رهگیری استخدام (ATS) افق ایرانیان خوش آمدید.
                         </h2>
 
                     </div>
@@ -149,41 +174,38 @@ const Login = () => {
                                     <label htmlFor="chk" aria-hidden="true">ورود</label>
                                     <Field disabled={phoneentered} className='yekan' type="text" id="phone" name="phone"
                                            placeholder="شماره تلفن"/>
-                                    <ErrorMessage style={{textAlign:'center' , color:'white'}} component='p' className='yekan-regular '
+                                    <ErrorMessage style={{textAlign: 'center', color: 'white'}} component='p'
+                                                  className='yekan-regular '
                                                   name='phone'/>
 
                                     {
-                                        phoneentered &&   <Field   className='yekan' type="tel" id="code" name="code"
-                                                                   placeholder="کد ارسال شده"
+                                        phoneentered && <Field className='yekan' type="tel" id="code" name="code"
+                                                               placeholder="کد ارسال شده"
 
 
                                         />
                                     }
 
                                     <ErrorMessage component='span' className='has-text-danger yekan' name='pswd'/>
-                                    <button disabled={loading} className='my-4' type='submit' >
+                                    <button disabled={loading} className='my-4' type='submit'>
                                         {
-                                            loading ? <Spinner/> : phoneentered ? 'ورود' :'ارسال کد'
+                                            loading ? <Spinner/> : phoneentered ? 'ورود' : 'ارسال کد'
                                         }
-
 
 
                                     </button>
 
                                     {
-                                        phoneentered && <button onClick={()=>{
+                                        phoneentered && <button onClick={() => {
                                             setphoneentered(false);
 
-                                        }}  style={{backgroundColor:'rgba(88,86,86,0.39)' , border:'1px solid white'}}>
-                                        ویرایش/تغییر شماره
+                                        }} style={{backgroundColor: 'rgba(88,86,86,0.39)', border: '1px solid white'}}>
+                                            ویرایش/تغییر شماره
                                         </button>
                                     }
 
 
-
-
                                 </Form>
-
 
 
                             )}
--- src/pages/Notfound.jsx
+++ src/pages/Notfound.jsx
@@ -3,7 +3,7 @@ const Notfound = () => {
         <>
 
 
-            <div style={{
+            <div className='columns m-0 p-5' style={{
                 height: '100dvh',
                 display: 'flex',
                 justifyContent: 'center',
@@ -13,6 +13,20 @@ const Notfound = () => {
                 backgroundSize: 'cover'
 
             }}>
+                <div className='column is-12 clrseventext is-flex is-align-items-center is-justify-content-center'
+                     style={{
+                         minHeight: '10rem',
+                         backgroundColor: 'rgba(0,0,0,0.69)',
+
+
+                         backdropFilter: 'blur(0.1rem)',
+                         borderRadius: '0.6rem'
+                     }}>
+                    <p className='yekan-regular has-text-centered is-size-4 has-text-weight-bold clrfourtext'>
+                        صفحه مورد نظر شما یافت نشد !
+                    </p>
+
+                </div>
 
             </div>
 
